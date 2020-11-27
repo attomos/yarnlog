9edc9a54179c6ab052b765b73e920aa763c3c4ee
Nattaphoom Chaipreecha <attomos@gmail.com>
2020-11-28 06:00:40 +0700

    test: improve test coverage

--- tests/test_yarnlog.py
+++ tests/test_yarnlog.py
@@ -1,22 +1,19 @@
-from datetime import datetime
 from pathlib import Path
 
 import requests_mock
+from freezegun import freeze_time
 
-from yarnlog.main import download_and_extract, extract, save_file
-
-MOCK_DATETIME = datetime(year=2020, month=11, day=22)
+from yarnlog.main import download_and_extract, extract, pipeline, save_file
 
 
 def test_extract():
     text = """\
-<pre>
-2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
+<pre>2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
 2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from environment variable: null
 </pre>"""
-    expected = """
+    expected = """\
 2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
 2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
@@ -41,15 +38,13 @@ def test_extract_witout_pre_tag():
 def test_download_and_extract():
     with requests_mock.Mocker() as m:
         mock_response_text = """\
-<pre>
-2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
+<pre>2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
 2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from environment variable: null
 </pre>"""
         expected = """\
 EXTRACTED FROM http://test.com/application_1234
-
 2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
 2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
 2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
@@ -87,3 +82,28 @@ def test_save_file():
     expected = "some text"
     assert actual == expected
     output_path.unlink()
+
+
+@freeze_time("2020-11-28 05:34:36.112233", tz_offset=0)
+def test_pipeline():
+    with requests_mock.Mocker() as m:
+        mock_response_text = """\
+<pre>2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
+2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
+2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
+2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from environment variable: null
+</pre>"""
+        expected = """\
+EXTRACTED FROM http://test.com/application_0534
+2020-11-22 22:50:27.490 [main] DEBUG org.apache.hadoop.util.Shell - setsid exited with exit code 0
+2020-11-22 22:50:27.514 [main] DEBUG org.apache.hadoop.security.Groups - Group mapping impl=org.apache.hadoop.security.ShellBasedUnixGroupsMapping; cacheTimeout=300000; warningDeltaMs=5000
+2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from system property: null
+2020-11-22 22:50:27.542 [main] DEBUG org.apache.hadoop.metrics2.impl.MetricsSystemImpl - from environment variable: null
+"""
+        yarn_url = "http://test.com/application_0534"
+        m.get(yarn_url, text=mock_response_text)
+        pipeline(yarn_url)
+        output_path = Path("yarnlog_2020_11_28_053436__112233")
+        actual = output_path.read_text()
+        assert actual == expected
+        output_path.unlink()
